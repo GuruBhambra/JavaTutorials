2fad64ae8b6974a182fa27120a427e413b497d5c
GuruBhambra <gurupreet108@gmail.com>
2023-02-01 22:49:32 +0530

    Linked List and HashTable

--- src/ListCollections/HashTableconcept.java
+++ src/ListCollections/HashTableconcept.java
@@ -2,6 +2,8 @@ package ListCollections;
 
 import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class HashTableconcept {
 
@@ -19,7 +21,7 @@ public class HashTableconcept {
 		 Enumeration e = h1.elements();
 		 while(e.hasMoreElements()) {
 			Object aa = e.nextElement();
-			 System.out.println(aa);
+			 //System.out.println(aa);
 		 }
 		 
 			Hashtable<Integer, String> h2 = new Hashtable<>();
@@ -31,16 +33,42 @@ public class HashTableconcept {
 			
 			if(h2.equals(h1)) {
 				
-				System.out.println("both are equal");
+				//System.out.println("both are equal");
 			}
 			else {
-				System.out.println("Not equal");
+				//System.out.println("Not equal");
 			}
 			
 			Hashtable h3 = new Hashtable();
 	
 			 h3= (Hashtable) h1.clone();
-			 System.out.println(h3);
+			// System.out.println(h3);
+			 
+			 
+			 Hashtable<Integer,Employee> h = new Hashtable<>();
+			 
+			 Employee e1 = new Employee("Guru", 20, "qa");
+			 Employee e2 = new Employee("GuruSingh", 230, "qaaseasf");
+			 Employee e3 = new Employee("GuruBhamra", 210, "qefweafsfsa");
+			 Employee e4 = new Employee("Guru", 2330, "qasfsrf");
+			 
+			 h.put(1, e1);
+			 h.put(2, e2);
+			 h.put(3, e3);
+			 h.put(4, e4);
+			 
+			 Enumeration<Employee> em = h.elements();
+			 while (em.hasMoreElements()) {
+				Employee emp =  em.nextElement();
+				
+				System.out.println(emp.name + " " + emp.age); 
+	}
+//				Set<Entry<Integer, Employee>> s = h.entrySet();
+//				for(Entry<Integer, Employee> s1:s) {
+//					Employee obj =  s1.getValue();
+//				System.out.println(obj.name + " " + obj.age + " " + obj.dept);
+//				}
+
 	}
 
 }
--- src/ListCollections/LinkedListConcept.java
+++ src/ListCollections/LinkedListConcept.java
@@ -1,21 +1,31 @@
 package ListCollections;
 
 import java.util.LinkedList;
+import java.util.Map.Entry;
 
 public class LinkedListConcept {
 
 	public static void main(String[] args) {
 		
-		LinkedList<String> ll = new LinkedList<String>();
+		LinkedList<Employee> ll = new LinkedList<Employee>();
 		
-		ll.add("guru");
-		ll.add("singh");
-		ll.add("bhambra");
-		System.out.println(ll.get(0));
+//		ll.add("guru");
+//		ll.add("singh");
+//		ll.add("bhambra");
+//		System.out.println(ll.get(0));
+//		
+//		ll.addFirst("Mr.");
+//		ll.addLast("Thank u");
+//		System.out.println(ll);
 		
-		ll.addFirst("Mr.");
-		ll.addLast("Thank u");
-		System.out.println(ll);
+		Employee e1 = new Employee("Tom", 20, "sgfsef");
+		Employee e2 = new Employee("GURU", 220, "ADAEDGYAEDG");
+		
+		ll.add(e1);
+		ll.add(e2);
+		Employee ee =ll.element();
+		
+		System.out.println(ee.name);
 		
 		
 
